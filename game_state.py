dd32a72832ae387d0f37ed69b8c3b5735cb80fde
KimyCDM <kimy@educ.college-montreal.qc.ca>
2025-06-02 09:57:21 -0400

    Update game_state.py

--- game_state.py
+++ game_state.py
@@ -2,10 +2,10 @@ from enum import Enum
 
 
 class GameState(Enum):
-   MAIN_MENU = 0
-   OPTION = 1
-   GAME_NOT_STARTED = 2
-   GAME_STARTED = 3
-   GAME_OVER = 4
-   PAUSED = 5
-   LEADER_BOARD = 6
+    MAIN_MENU = 0
+    OPTION = 1
+    GAME_NOT_STARTED = 2
+    GAME_STARTED = 3
+    GAME_OVER = 4
+    PAUSED = 5
+    LEADER_BOARD = 6
